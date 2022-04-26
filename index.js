7f4281f1dd818124cc203fd962d22df36cd1beb4
jadeleafmoon <jadeleafmoon@gmail.com>
2022-04-26 20:57:58 +0900

    feat: add GET /products

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+app.get("/products", (req, res) => {
+    res.status(200).send(products);
+});
+
 app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));

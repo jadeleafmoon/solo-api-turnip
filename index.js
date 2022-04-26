ae0b47c8d48c0a41a396c7ded56c876ebe75828b
jadeleafmoon <jadeleafmoon@gmail.com>
2022-04-26 21:25:51 +0900

    feat: add PATCH /products endpoint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { products } = require('./data/products.json');
 const express = require('express');
 const app = express();
 app.use(express.json());
-const PORT = 3000;
+const PORT = 4040;
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
@@ -19,6 +19,19 @@ app.post('/products', (req, res) => {
 	const newProduct = req.body;
 	products.push(newProduct);
 	res.status(200).send(`The product ${newProduct.name} has been added.`)
+});
+
+app.patch('/products/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const updates = req.body;
+
+    const foundProduct = products.find(product => product.id === id);
+    
+    for (const key in updates) {
+        foundProduct[key] = updates[key];
+    }
+
+    res.status(200).send(`The product with id = ${id} has been updated`);
 });
 
 app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));

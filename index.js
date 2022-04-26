95877a524e96da511e99705198521e28eff43f48
jadeleafmoon <jadeleafmoon@gmail.com>
2022-04-26 21:07:53 +0900

    feat: add POST /products enpoint

--- index.js
+++ index.js
@@ -2,17 +2,23 @@
 
 const { products } = require('./data/products.json');
 
-
 const express = require('express');
 const app = express();
+app.use(express.json());
 const PORT = 3000;
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-app.get("/products", (req, res) => {
-    res.status(200).send(products);
+app.get('/products', (req, res) => {
+	res.status(200).send(products);
+});
+
+app.post('/products', (req, res) => {
+	const newProduct = req.body;
+	products.push(newProduct);
+	res.status(200).send(`The product ${newProduct.name} has been added.`)
 });
 
 app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));

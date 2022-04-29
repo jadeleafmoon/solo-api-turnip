e3147a79c5a7c42bdc56846d906ea7c2d9bfd095
jadeleafmoon <jadeleafmoon@gmail.com>
2022-04-29 21:39:08 +0900

    feat: extract routes for /products to /api/products

---
+++ routes/api.js
@@ -0,0 +1,70 @@
+// The base of this router is /api/
+
+const express = require('express');
+const router = express.Router();
+
+// initialize Knex
+const knexConfig = require('../knexfile');
+const config = knexConfig[process.env.NODE_ENV || 'development'];
+const knex = require('knex')(config);
+
+// Endpoints
+router.get('/', (req, res) => {
+	res.send('Hello and welcome to the Turnip API!');
+});
+
+// This route would be /api/products
+router.get('/products', (req, res) => {
+	knex('products')
+		.select({
+			id    : 'id',
+			name  : 'name',
+			price : 'price'
+		})
+		.then((result) => {
+			res.status(200).send(result);
+		});
+});
+
+router.post('/products', (req, res) => {
+	const newProduct = req.body;
+
+	knex('products').insert(newProduct).returning('*').then((result) => {
+		res.status(201).send(`The product ${result[0].name} has been added`);
+	});
+});
+
+router.patch('/products/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	const productUpdates = req.body;
+
+	knex('products')
+		.where('id', '=', id)
+		.update(productUpdates)
+		.returning('*')
+		.then((result) => {
+			res
+				.status(200)
+				.send(
+					`The product ${productUpdates.name} with id ${id} has been updated`
+				);
+		});
+});
+
+router.delete('/products/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+
+	return knex('products')
+		.where('id', '=', id)
+		.del()
+		.returning('*')
+		.then((result) => {
+			console.log('🔥 DEL result:', result);
+			res
+				.status(200)
+				.send(`The product ${result[0].name} with id ${id} has been deleted`);
+		});
+
+});
+
+module.exports = router;
